9a22cb0c0521c6c15bc21d50a5bb5fe99071229e
mingliangfu123 <mingliang.fu@valtech.com>
2024-07-26 14:37:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World mingliang!');
+  response.end('Hello World mingliang 8!');
 };
 var www = http.createServer(handleRequest);
 www.listen(80);
